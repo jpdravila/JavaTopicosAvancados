dc336104505ca8d5e7b2784daf1f67a5802cd243
João Pedro de Ávila <jpdravila@gmail.com>
2022-10-20 21:37:53 -0400

    Base64

---
+++ src/main/java/github/io/jpdravila/BaseMeiaQuatro/Base64Exemplo.java
@@ -0,0 +1,24 @@
+package github.io.jpdravila.BaseMeiaQuatro;
+
+import java.util.Base64;
+
+public class Base64Exemplo {
+
+    public static void main(String[] args){
+
+        try {
+            String texto = "A classe Base64 no Java 08";
+
+            String textoCodificado = Base64.getEncoder().encodeToString(texto.getBytes("utf-8"));
+
+            String textoDecodificado = new String(Base64.getDecoder().decode(textoCodificado),"utf-8");
+
+            System.out.println(textoCodificado);
+            System.out.println(textoDecodificado);
+
+        } catch (Exception e){
+            System.out.println("Error!");
+        }
+    }
+
+}
