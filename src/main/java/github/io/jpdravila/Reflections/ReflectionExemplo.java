438d28dd59782e9d82819da79e2d552a6a5215ae
João Pedro de Ávila <jpdravila@gmail.com>
2022-10-25 20:50:22 -0400

    Reflexao de metodos e parametros

---
+++ src/main/java/github/io/jpdravila/Reflections/ReflectionExemplo.java
@@ -0,0 +1,53 @@
+package github.io.jpdravila.Reflections;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+class Produto{
+    public String nome;
+    public Double preco;
+
+    public Produto(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
+
+public class ReflectionExemplo {
+
+    public static void main(String[] args){
+        //Instância da classe produto
+        Produto p = new Produto("Geladeira", 3000.00);
+        //Instância da classe class
+        Class cl = p.getClass();
+        //Todos os métodos declarados na classe produto
+        Method[] method = cl.getDeclaredMethods();
+
+        for(Method m : method){
+            System.out.println(m.getName());
+
+            Parameter[] parameter = m.getParameters();
+
+            for(Parameter pr : parameter){
+                System.out.println(pr);
+            }
+        }
+    }
+
+}
