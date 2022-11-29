fb007f47bd0f66e3d335d16d7066a0257d886437
João Pedro de Ávila <jpdravila@gmail.com>
2022-11-28 21:16:18 -0400

    Teeling Collectors

---
+++ src/main/java/github/io/jpdravila/Java12/TeeingCollector/TeeingCollectorExemplo.java
@@ -0,0 +1,19 @@
+package github.io.jpdravila.Java12.TeeingCollector;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TeeingCollectorExemplo {
+    public static void main(String[] args){
+
+        // Collectors.teeing(soma, qtde, media);
+
+        double resultado = Stream.of(50, 10, 3, 19, 11, 22, 19).collect(
+                Collectors.teeing(Collectors.summingDouble(i -> i),
+                        Collectors.counting(), (soma, qtde) -> soma / qtde)
+        );
+
+        System.out.println(resultado);
+
+    }
+}
